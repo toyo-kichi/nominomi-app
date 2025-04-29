faaf5adf175bed5af231154ed1c2fee028fd6844
toyo-kichi <koki0515baske@gmail.com>
2025-04-29 12:16:29 +0900

    feat: install Express

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+// server/src/index.ts
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3001; // フロントは3000番、サーバーは3001番にする
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Expressサーバー動いてるよ！");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

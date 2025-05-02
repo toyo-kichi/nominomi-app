5f8ef2a294f44e495f5cd1db8e19d69dc29ee191
toyo-kichi <koki0515baske@gmail.com>
2025-05-02 21:59:53 +0900

    Add: /login API with mock group data

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,29 @@ const PORT = 3001; // フロントは3000番、サーバーは3001番にする
 app.use(cors());
 app.use(express.json());
 
+interface mockGroupData {
+  [key: string]: string;
+}
+
+const mockGroupData: mockGroupData = {
+  group123: "pass123",
+  teamA: "secret456",
+};
+
 app.get("/", (req, res) => {
   res.send("Expressサーバー動いてるよ！");
+});
+
+app.post("/login", (req, res) => {
+  const { groupId, passcode } = req.body;
+
+  const validPasscode = mockGroupData[groupId];
+
+  if (validPasscode && validPasscode === passcode) {
+    return res.json({ success: true, token: "mock-token-12345" });
+  } else {
+    return res.status(401).json({ success: false, message: "ログイン失敗" });
+  }
 });
 
 app.listen(PORT, () => {
